424bfdf5b0724d2bad4c00f73f89d5891882c8fa
deniskimani <48308527+deniskimani@users.noreply.github.com>
2023-03-06 23:43:28 +0300

    small change in env-helper

--- scripts/env-helper.js
+++ scripts/env-helper.js
@@ -4,9 +4,14 @@ function decideEnv() {
   // this function decides whether to load .env or .env.test.
   if (NODE_ENV != 'production') {
     // capture first command line argument passed to this script
+
     const args = process.argv.slice(2)[0];
 
     const envFile = args === 'test' ? '../.env.test' : '../.env';
+
+    // put the next line to explain whats in args
+
+    console.log(envFile);
 
     require('dotenv').config({
       path: path.join(__dirname, envFile),
